da70e2e2fc42870960238960dc8fef3714cdf42a
dakur17 <s160819001@student.ubaya.ac.id>
2024-03-25 21:05:49 +0700

    env updated

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# buat docker images dari docker file
+docker build -t docker.pkg.github.com/dakur17/a433-microservices/dakur17/karsajobs-ui:latest .
+# login ke githubpackages
+docker login docker.pkg.github.com -u dakur17 -p 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
+# push ke githubpackages
+docker push docker.pkg.github.com/dakur17/a433-microservices/dakur17/karsajobs-ui:latest
